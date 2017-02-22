4c1f4e2d5797c0feb35f657b14f79568a7cdc5ce
Sam <skriegman@gmail.com>
2017-02-22 13:58:01 -0500

    more vacc stuff

--- evolution.py
+++ evolution.py
@@ -5,9 +5,6 @@ from replicators import Population
 
 
 SEED = int(sys.argv[1])
-START_RUN_IDX = float(sys.argv[2])
-END_RUN_IDX = int(sys.argv[3])
-# NUM_RUNS = 30
 
 POP_SIZE = 30
 GENS = 1000
@@ -16,24 +13,23 @@ EVAL_TIME = 500
 FIT_STAT = np.min
 
 
-for run in range(START_RUN_IDX, END_RUN_IDX):
-    for develop in [True, False]:
+for develop in [True, False]:
 
-        pop = Population(POP_SIZE, num_env=NUM_ENV, development=develop, fitness_stat=FIT_STAT)
+    pop = Population(POP_SIZE, num_env=NUM_ENV, development=develop, fitness_stat=FIT_STAT)
 
-        for gen in range(GENS):
-            pop.create_children_through_mutation()
-            pop.add_random_inds(1)
-            pop.increment_ages()
-            pop.evaluate()
-            pop.reduce()
-            pop.print_non_dominated()
-            pop.gen += 1
+    for gen in range(GENS):
+        pop.create_children_through_mutation()
+        pop.add_random_inds(1)
+        pop.increment_ages()
+        pop.evaluate()
+        pop.reduce()
+        pop.print_non_dominated()
+        pop.gen += 1
 
-        exp_name = "Devo" if develop else "Evo"
-        f = open('data/Exp_{0}_Run_{1}.p'.format(exp_name, run), 'w')
-        pickle.dump(pop, f)
-        f.close()
+    exp_name = "Devo" if develop else "Evo"
+    f = open('data/Exp_{0}_Run_{1}.p'.format(exp_name, SEED), 'w')
+    pickle.dump(pop, f)
+    f.close()
 
 
 # r = open('data/Exp_Devo_Run_0.p', 'r')
---
+++ submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+	echo "Number of runs not specified"
+	exit 1
+fi
+echo "Using number of runs: " $1
+
+for x in `seq 1 $2`
+do
+	seed=${x}
+	qsub -vARG_SEED=${seed} $HOME/pkg/pyrosim/single_runner.pbs
+	echo "Run $x started with seed $seed"
+	sleep 1;
+done
