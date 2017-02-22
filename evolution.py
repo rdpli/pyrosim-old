ef28e5ca6424b96a826afcc62e57841dde47d08e
Sam <skriegman@gmail.com>
2017-02-21 21:16:55 -0500

    for vacc

--- evolution.py
+++ evolution.py
@@ -1,36 +1,42 @@
-from replicators import Population
 import pickle
+import numpy as np
+from replicators import Population
+
 
 POP_SIZE = 30
 GENS = 1000
 NUM_ENV = 4
 EVAL_TIME = 500
-DEVO = False
-
-pop = Population(POP_SIZE, num_env=NUM_ENV, development=DEVO)
-
-for gen in range(GENS):
-    pop.create_children_through_mutation()
-    pop.add_random_inds(1)
-    pop.increment_ages()
-    pop.evaluate()
-    pop.reduce()
-    pop.print_non_dominated()
-    pop.gen += 1
+FIT_STAT = np.min
+NUM_RUNS = 30
 
 
-f = open('robot.p', 'w')
-pickle.dump(pop, f)
-f.close()
+for run in range(NUM_RUNS):
+    for develop in [True, False]:
+
+        pop = Population(POP_SIZE, num_env=NUM_ENV, development=develop, fitness_stat=FIT_STAT)
+
+        for gen in range(GENS):
+            pop.create_children_through_mutation()
+            pop.add_random_inds(1)
+            pop.increment_ages()
+            pop.evaluate()
+            pop.reduce()
+            pop.print_non_dominated()
+            pop.gen += 1
+
+        f = open('data/{0}_robot_{1}.p'.format(develop, run), 'w')
+        pickle.dump(pop, f)
+        f.close()
 
 
-r = open('robot.p', 'r')
-final_pop = pickle.load(r)
-
-sorted_inds = sorted(final_pop.individuals_dict, key=lambda k: final_pop.individuals_dict[k].fitness)
-final_pop.individuals_dict[sorted_inds[-1]].start_evaluation(blind=False, eval_time=EVAL_TIME, pause=True)
-
-r.close()
+# r = open('robot.p', 'r')
+# final_pop = pickle.load(r)
+#
+# sorted_inds = sorted(final_pop.individuals_dict, key=lambda k: final_pop.individuals_dict[k].fitness)
+# final_pop.individuals_dict[sorted_inds[-1]].start_evaluation(blind=False, eval_time=EVAL_TIME, pause=True)
+#
+# r.close()
 
 
 
