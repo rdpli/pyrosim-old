748856c9cd086e21b94692e5143254048e84cd5d
Sam <skriegman@gmail.com>
2017-06-05 19:28:18 -0400

    separate file for laptop and vacc

---
+++ evo_single_run.py
@@ -0,0 +1,48 @@
+import sys
+import pickle
+import random
+import numpy as np
+from replicators import Population
+
+
+SEED = int(sys.argv[1])
+
+POP_SIZE = 30
+GENS = 1000
+NUM_ENV = 2
+EVAL_TIME = 1000
+FIT_STAT = np.sum
+
+
+random.seed(SEED)
+np.random.seed(SEED)
+
+for development_type in [0, 1]:
+
+    pop = Population(POP_SIZE, num_env=NUM_ENV, development_type=development_type, fitness_stat=FIT_STAT)
+
+    for gen in range(GENS):
+        pop.create_children_through_mutation()
+        pop.add_random_inds(1)
+        pop.increment_ages()
+        pop.evaluate()
+        pop.reduce()
+        pop.print_non_dominated()
+        pop.gen += 1
+
+    f = open('data/Dev_Compress_{0}_Run_{1}.p'.format(development_type, SEED), 'w')
+    pickle.dump(pop, f)
+    f.close()
+
+
+r = open('data/Dev_1_Run_1.p', 'r')
+final_pop = pickle.load(r)
+
+sorted_inds = sorted(final_pop.individuals_dict, key=lambda k: final_pop.individuals_dict[k].fitness)
+final_pop.individuals_dict[sorted_inds[-1]].start_evaluation(blind=False, eval_time=EVAL_TIME, pause=True)
+
+r.close()
+
+
+
+
--- evolution.py
+++ evolution.py
@@ -1,43 +1,46 @@
-import sys
 import pickle
+import random
 import numpy as np
 from replicators import Population
 
 
-SEED = int(sys.argv[1])
-
 POP_SIZE = 30
 GENS = 1000
 NUM_ENV = 2
-EVAL_TIME = 500
+EVAL_TIME = 1000
 FIT_STAT = np.sum
 
 
-for development_type in [0, 1]:
+for run in range(30):
 
-    pop = Population(POP_SIZE, num_env=NUM_ENV, development_type=development_type, fitness_stat=FIT_STAT)
+    random.seed(run)
+    np.random.seed(run)
 
-    for gen in range(GENS):
-        pop.create_children_through_mutation()
-        pop.add_random_inds(1)
-        pop.increment_ages()
-        pop.evaluate()
-        pop.reduce()
-        pop.print_non_dominated()
-        pop.gen += 1
+    for development_type in [0, 1]:
 
-    f = open('data/Dev_{0}_Run_{1}.p'.format(development_type, SEED), 'w')
-    pickle.dump(pop, f)
-    f.close()
+        pop = Population(POP_SIZE, num_env=NUM_ENV, development_type=development_type, fitness_stat=FIT_STAT)
+
+        for gen in range(GENS):
+            pop.create_children_through_mutation()
+            pop.add_random_inds(1)
+            pop.increment_ages()
+            pop.evaluate()
+            pop.reduce()
+            pop.print_non_dominated()
+            pop.gen += 1
+
+        f = open('data/Dev_Compress_{0}_Run_{1}.p'.format(development_type, run), 'w')
+        pickle.dump(pop, f)
+        f.close()
 
 
-r = open('data/Dev_1_Run_1.p', 'r')
-final_pop = pickle.load(r)
-
-sorted_inds = sorted(final_pop.individuals_dict, key=lambda k: final_pop.individuals_dict[k].fitness)
-final_pop.individuals_dict[sorted_inds[-1]].start_evaluation(blind=False, eval_time=EVAL_TIME, pause=True)
-
-r.close()
+# r = open('data/Dev_1_Run_1.p', 'r')
+# final_pop = pickle.load(r)
+#
+# sorted_inds = sorted(final_pop.individuals_dict, key=lambda k: final_pop.individuals_dict[k].fitness)
+# final_pop.individuals_dict[sorted_inds[-1]].start_evaluation(blind=False, eval_time=EVAL_TIME, pause=True)
+#
+# r.close()
 
 
 
